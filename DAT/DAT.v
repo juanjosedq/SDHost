9b18a4bd9ba8afa2b2021f1b92e3adfd0cd61297
alfaro05 <3alberto1alfaro4@gmail.com>
2016-11-15 18:58:00 +0000

    Update DAT.v

--- DAT/DAT.v
+++ DAT/DAT.v
@@ -1,12 +1,12 @@
 module DAT (buffer_in, reg_vector, card_out, clk, pop, push);
 
-parameter vector_width = 10;
+parameter vector_width = 10;			//reg_vector has inputs with information coming from registers
 input wire [31:0] buffer_in;
-input wire [vector_width - 1 :0] reg_vector;
-output wire [3:0] card_out;
-input wire pop, clk;
+input wire [vector_width - 1 :0] reg_vector;	//there will be a specific index for reach element of the array
+output wire [3:0] card_out;			//in reg_vector, set with parameters with a +20 offset
+input wire pop, clk;				//as parameters below 20 will be set for states 
 output reg push;
-reg state = 0;
+reg state [3:0]= 0;
 
 
 //States declaration:
@@ -17,6 +17,11 @@ parameter fifo_check = 2;
 parameter trans = 3;		//send or recieve state
 parameter trans_ready = 4;	//transference acknolgedge state
 
+//reg_vector index parameters: (real index will be "parameter" - 20)
+
+parameter send
+
+parameter 
 always @ (posedge clk)
 	begin 
 		case (state)
@@ -45,5 +50,9 @@ always @ (posedge clk)
 					end
 				end	
 		endcase	
+	end
+	
+	if (state == trans) begin
+			
 	end
 endmodule
