ac5f9281e7d6a0af4136720ede98168595da2506
juanjosedq <juanjose.dq@gmail.com>
2016-11-16 16:52:03 -0600

    2 wrapper

---
+++ wrapper_serial_paralelo/testbench.v
@@ -0,0 +1,22 @@
+module testbench;                                // Se define un modulo de prueba
+
+	parameter n = 7; //paralell signal size at output
+	wire input_1;
+	wire clk;
+	wire enable;
+	wire reset;
+	wire [n-1:0] output_n;
+	wire push;
+
+  initial begin                             // Bloque que se ejecuta al inicio
+     $dumpfile("testbench.vcd");                 // El resultado se guarda en test.vcd
+     $dumpvars(0,testbench);
+  end
+       
+
+  probador valores (input_1, clk, enable, reset);
+
+  wrapper_serial_paralelo c1 (output_n, input_1, clk, enable, reset, push); 
+   
+
+endmodule // test
