00457bea98cb3d06b32e9f0e17edf7f9b87f1745
alfaro05 <3alberto1alfaro4@gmail.com>
2016-11-18 11:30:34 +0000

    Correcion maquina de estados

--- DAT/DAT.v
+++ DAT/DAT.v
@@ -1,12 +1,12 @@
-module DAT (buffer_in, reg_vector, card_out, clk, pop, push);
+module DAT (buffer_in, reg_vector, card_out, clk, pop, push, fifo_ack);
 
-parameter vector_width = 10;			//reg_vector has inputs with information coming from registers
+parameter vector_width = 6;			//reg_vector has inputs with information coming from registers
 input wire [31:0] buffer_in;
 input wire [vector_width - 1 :0] reg_vector;	//there will be a specific index for reach element of the array
 output wire [3:0] card_out;			//in reg_vector, set with parameters with a +20 offset
-input wire pop, clk;				//as parameters below 20 will be set for states 
+input wire pop, clk, fifo_ack;				//as parameters below 20 will be set for states 
 output reg push;
-reg state [3:0]= 0;
+reg [3:0] state = 0;
 
 
 //States declaration:
@@ -19,40 +19,63 @@ parameter trans_ready = 4;	//transference acknolgedge state
 
 //reg_vector index parameters: (real index will be "parameter" - 20)
 
-parameter send
+parameter present_state = 20; 	//includes multiple or single block mode
+				//select bit, data direction selec 					
+				//bit, auto CMD12 enable and block 					
+				//count enable bit 
+parameter host_control = 24;	//high-low speed select
+parameter trans_block_size = 25;
+parameter command_reg = 26;	//commands through DAT
+parameter trans_mode = 27;	//same info as in present state
+parameter software_reset = 28;	
 
-parameter 
 always @ (posedge clk)
 	begin 
 		case (state)
 			reset:
 				begin
-					state = idle;
+					state <= idle;
 				end
 			idle:
 				begin
-					state = fifo_check;
+					if (reset) begin 
+						state <= reset;
+					end else begin 
+						state <= fifo_check;
+					end
 				end
 			fifo_check:
 				begin
-					state = trans;
+					if (reset) begin 
+						state <= reset;
+					end else begin 
+						if (fifo_ack) begin
+							state <= trans;
+						end else begin
+							state <= fifo_check;
+						end
+					end
 				end
 			trans:
 				begin
-					state = trans_ready;
+					if (reset) begin 
+						state <= reset;
+					end else begin 
+						state <= trans_ready;
+					end
 				end
 			trans_ready:
 				begin
 					if(reset) begin
-						state = reset;
+						state <= reset;
 					end else begin
-						state = trans_ready;
+						state <= trans_ready;
 					end
 				end	
 		endcase	
 	end
 	
 	if (state == trans) begin
-			
+		communication com_settings(buffer_in, card_out, clk, pop, push, reg_vector[present_state - 20], reg_vector[present_state -20 +1], reg_vector[trans_block_size - 20]); 		
 	end
 endmodule
