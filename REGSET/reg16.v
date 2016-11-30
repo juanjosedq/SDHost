4bebb6ebd8bb7f6d1d659344749c45742e1ac6c7
Pablo Delgado <pablo94dc@gmail.com>
2016-11-30 15:33:06 -0600

    reg1

--- REGSET/reg16.v
+++ REGSET/reg16.v
@@ -278,7 +278,7 @@ module reg_00E(
 	ResponseTypeSelect_out,
 	// sdma_sysadd_h,
 	// sdma_sysadd_l,
-	;
+	);
 //Parameters before
 parameter width = 16;
 
@@ -288,38 +288,38 @@ parameter width = 16;
 input rst;						// reset
 input clk;						// clock
 
-input	[4:0] CommandIndex_in,
-input	[1:0] CommandType_in,
-input	DataPresentState_in,
-input	CommandIndezCheckEnable_in,
-input	CommandCRCCheckEnable_in,
-input	[1:0] ResponseTypeSelect_in,
+input	[4:0] CommandIndex_in;
+input	[1:0] CommandType_in;
+input	DataPresentState_in;
+input	CommandIndezCheckEnable_in;
+input	CommandCRCCheckEnable_in;
+input	[1:0] ResponseTypeSelect_in;
 
-output	[4:0] CommandIndex_out,
-output	[1:0] CommandType_out,
-output	DataPresentState_out,
-output	CommandIndezCheckEnable_out,
-output	CommandCRCCheckEnable_out,
-output	[1:0] ResponseTypeSelect_out,
+output	[4:0] CommandIndex_out;
+output	[1:0] CommandType_out;
+output	DataPresentState_out;
+output	CommandIndezCheckEnable_out;
+output	CommandCRCCheckEnable_out;
+output	[1:0] ResponseTypeSelect_out;
 
 //Port Types
 wire [(width-1):0] data_in;
 
 wire rst;		
 wire clk;
-wire	[4:0] CommandIndex_in,
-wire	[1:0] CommandType_in,
-wire	DataPresentState_in,
-wire	CommandIndezCheckEnable_in,
-wire	CommandCRCCheckEnable_in,
-wire	[1:0] ResponseTypeSelect_in,
+wire	[4:0] CommandIndex_in;
+wire	[1:0] CommandType_in;
+wire	DataPresentState_in;
+wire	CommandIndezCheckEnable_in;
+wire	CommandCRCCheckEnable_in;
+wire	[1:0] ResponseTypeSelect_in;
 
-wire	[4:0] CommandIndex_out,
-wire	[1:0] CommandType_out,
-wire	DataPresentState_out,
-wire	CommandIndezCheckEnable_out,
-wire	CommandCRCCheckEnable_out,
-wire	[1:0] ResponseTypeSelect_out,
+wire	[4:0] CommandIndex_out;
+wire	[1:0] CommandType_out;
+wire	DataPresentState_out;
+wire	CommandIndezCheckEnable_out;
+wire	CommandCRCCheckEnable_out;
+wire	[1:0] ResponseTypeSelect_out;
 
 reg ack;
 reg busy_out; 						// 
@@ -345,6 +345,104 @@ assign	DataPresentState_out	= data_out [5];
 assign	CommandIndezCheckEnable_out	= data_out [4];
 assign	CommandCRCCheckEnable_out	= data_out [3];
 assign	ResponseTypeSelect_out	= data_out [1:0];
+
+//Bloque combinacional
+always @(*) begin
+	// comportamiento del ack
+// if ((data_in == data_out)&& ~busy_in) begin
+	if (data_in == data_out) begin
+		ack = 1'b1;
+	end
+	else begin
+		ack = 1'b0;	
+	end
+	// comportamiento de busy
+	/*if (enb_block0 ^ enb_block1 ^ enb_block2) begin
+		busy_out = 1'b1;
+	end
+	else begin
+		busy_out = 1'b0;
+	end*/
+	// busy con or
+	// if ((enb_block0 && enb_block1) || (enb_block0 && enb_block2) || (enb_block2 && enb_block1) ||(enb_block0&&enb_block1&&enb_block2) ) begin
+	// 	busy_out = 1'b1;
+	// end
+	// else begin
+	// 	busy_out = 1'b0;
+	// end
+// 	if ((enb_block0 && enb_block1) || (enb_block0 && enb_block2) || (enb_block2 && enb_block1) ||(enb_block0&&enb_block1&&enb_block2) ) begin
+// 		busy_in = 1'b1;
+// 	end
+// 	else begin
+// 		busy_in = 1'b0;
+// 	end
+// end
+
+//Bloque secuencial
+always @(posedge clk or posedge rst) begin
+	if (rst) begin
+		// reset
+		data_out <= 32'b0;
+	end
+	else if (~busy_out ) begin
+		data_out <= data_in;
+	end
+	else if (~busy_out) begin
+		data_out <= data_in;
+	end
+	else if (~busy_out) begin
+		data_out <= data_in;
+	end
+	else begin
+		data_out <= data_out;
+	end
+end
+
+endmodule
+
+
+module reg_008(
+	clk,
+	rst,
+	ack,
+	busy_out,
+	CommandArgument_in,
+	CommandArgument_out,
+	// sdma_sysadd_h,
+	// sdma_sysadd_l,
+	);
+//Parameters before
+parameter width = 32;
+
+//INPUT & OUTPUT DECLARATION
+
+
+input rst;						// reset
+input clk;						// clock
+
+input	[1:0] CommandArgument_in;
+
+output	[1:0] CommandArgument_out;
+
+//Port Types
+wire [(width-1):0] data_in;
+
+wire rst;		
+wire clk;
+wire	[31:0] CommandArgument_in;
+wire	[31:0] CommandArgument_out;
+
+reg ack;
+reg busy_out; 						// 
+reg [(width-1):0] data_out;
+// reg sdma_sysadd_l;
+// reg sdma_sysadd_h;
+
+
+//CODE
+assign data_in  = CommandArgument_out ;		
+assign CommandArgument_out = data_out ;
+
 
 //Bloque combinacional
 always @(*) begin
